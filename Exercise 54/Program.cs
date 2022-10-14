57ad048fc4b139fec361d714a56842a0254a62dc
Sergey <bibert.shmibert@gmail.com>
2022-10-14 14:49:18 +0400

    add exercise 54 with library

--- Exercise 54/Program.cs
+++ Exercise 54/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using MyLibrary;
+
+void SortRowsToMin()
+{
+    Random random = new Random();
+    int rows = random.Next(3, 6);
+    int cols = random.Next(3, 6);
+    int[,] numbers = new int[rows, cols];
+    ArraysMethods.FillMultiArray(numbers, -10, 10);
+    ArraysMethods.PrintMultiArray(numbers);
+    Console.WriteLine();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++ )
+        {
+            int border = cols - j - 1;
+            for(int k = 0; k < border; k++)
+            {
+                if(numbers[i, k] < numbers[i, k + 1])
+                {
+                    (numbers[i, k], numbers[i, k + 1]) = (numbers[i, k + 1], numbers[i, k]);
+                }
+            }
+        }
+    }
+    ArraysMethods.PrintMultiArray(numbers);
+}
+
+SortRowsToMin();
