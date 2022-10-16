8ee85d2a74d6f0e0cca98f1bb514ff190e2d01fa
Sergey <bibert.shmibert@gmail.com>
2022-10-16 22:02:26 +0400

    add exercise 56 with library

---
+++ Exercise 56/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using MyLibrary;
+
+Random random = new Random();
+int rowsAmount = random.Next(4, 10);
+int colsAmount = random.Next(5, 5);
+int[,] numbers = new int[rowsAmount, colsAmount];
+
+void FindMinSum(int[,] arr)
+{
+    ArraysMethods.FillMultiArray(numbers, 0, 10);
+    ArraysMethods.PrintMultiArray(numbers);
+    Console.WriteLine();
+
+    int sumMin = 0;
+    int sum = 0;
+    int currentRow = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            sum += numbers[i, j];
+        }
+        Console.WriteLine($"сумма строки {i} - {sum}");
+        if (sum < sumMin || sumMin == 0)
+        {
+            sumMin = sum;
+            currentRow = i;
+        }
+        sum = 0;
+    }
+    Console.Write($"Наименьшая сумма элементов '{sumMin}' - строка {currentRow}");
+    Console.WriteLine();
+}
+
+FindMinSum(numbers);
---
+++ Exercise 56/library-CS.cs
@@ -0,0 +1,96 @@
+﻿namespace MyLibrary
+{
+    class ArraysMethods
+    {
+
+        // ---------- ДЛИННА ЧИСЛА ---------------
+
+        public static int GetLength(int num)
+        {
+            int count = 0;
+            while (num > 0)
+            {
+                num = num / 10;
+                count += 1;
+            }
+            return count;
+        }
+
+        // ---------- ПЕЧАТЬ ОДНОМЕРНОГО МАССИВА ---------------
+
+        public static void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        // ---------- ПЕЧАТЬ МНОГОМЕРНОГО МАССИВА ---------------
+
+        public static void PrintMultiArray(int[,] numbers)
+        {
+            int rows = numbers.GetLength(0);
+            int cols = numbers.GetLength(1);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(numbers[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        // ---------- ЗАПОЛНЕНИЕ ОДНОМЕРНОГО МАССИВА ---------------
+
+        public static void FillArray(int[] arr, int minValue, int maxValue)
+        {
+            maxValue++;
+            Random random = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(minValue, maxValue);
+            }
+        }
+
+        // ---------- ЗАПОЛНЕНИЕ МНОГОМЕРНОГО МАССИВА ---------------
+
+        public static void FillMultiArray(int[,] arr, int minValue, int maxValue)
+        {
+            maxValue++;
+            Random random = new Random();
+            int rows = arr.GetLength(0);
+            int cols = arr.GetLength(1);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    arr[i, j] = random.Next(minValue, maxValue);
+                }
+            }
+        }
+
+        // ---------- СОРТИРОВКА МАССИВА ---------------
+
+        public static void GetSorted(int[] array)
+        {
+            int temp;
+            for (int i = 1; i < array.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (Math.Abs(array[i]) < Math.Abs(array[j])) 
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                    Console.Write($"[{i}] < [{j}]");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
